3cebdcbfe078f94d3c69c9ec7c3c6b8e5c594647
Danny-C21 <dannychia51990@gmail.com>
2021-10-30 11:58:10 -0500

    Added stocks.js

---
+++ data.js/stocks.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    var dataPoints = [];
+    var stockChart = new CanvasJS.StockChart("stockChartContainer", {
+      exportEnabled: true,
+      title: {
+        text:"Netflix Stock"
+      },
+      subtitles: [{
+        text:"Stock Price from IPO to current"
+      }],
+      charts: [{
+        axisX: {
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+            valueFormatString: "MMM YYYY"
+          }
+        },
+        axisY: {
+          title: "Stock price in Dollar",
+          prefix: "$",
+          suffix: "",
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+            valueFormatString: "$#,###.00M",
+          }
+        },
+        data: [{
+          type: "line",
+          xValueFormatString: "MMM YYYY",
+          yValueFormatString: "$#,###.##M",
+          dataPoints : dataPoints
+        }]
+      }],
+      navigator: {
+        slider: {
+          minimum: new Date(2002, 04, 23),
+          maximum: new Date(2021, 11, 25)
+        }
+      }
+    });
+    $.getJSON("json.loads(results)", function(data) {
+      for(var i = 0; i < data.length; i++){
+        dataPoints.push({x: new Date(data[i].date), y: Number(data[i].sale)});
+      }
+      stockChart.render();
+    });
+  }
